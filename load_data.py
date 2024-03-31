8e0812d22b6df27ed7698050a80195c860c88463
chathuranga-jayanath <chathuranga.19@cse.mrt.ac.lk>
2024-03-31 20:34:29 +0530

    change dataset name

--- load_data.py
+++ load_data.py
@@ -2,5 +2,5 @@ from datasets import load_dataset
 
 dataset_name = "princeton-nlp/SWE-bench"
 dataset = load_dataset(dataset_name)
-
-print(dataset)
+train_dataset = dataset['train']
+print(train_dataset)
